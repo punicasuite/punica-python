92a8de9aa8207d44197289e66a7b94f155939d86
lucas7788 <sishsh@163.com>
2018-10-16 15:50:03 +0800

    fix list_cmd bug

--- punica/list/list_punica.py
+++ punica/list/list_punica.py
@@ -1,4 +1,5 @@
-import os
+
+from punica.common.define import DEFAULT_CONFIG
 
 from punica.utils.handle_config import handle_invoke_config
 
@@ -6,18 +7,12 @@ from punica.utils.handle_config import handle_invoke_config
 class ListPunica:
     @staticmethod
     def list_funcs(project_dir: str, config_name: str):
-        config_dir = os.path.join(project_dir, 'contracts')
         if config_name == '':
-            config_dir = os.path.join(config_dir, 'default-config.json')
-        else:
-            config_dir = os.path.join(config_dir, config_name)
-        if not os.path.exists(config_dir):
-            print(config_dir, " not exist")
-        else:
-            invoke_config, password_config = handle_invoke_config(project_dir, 'default-config.json')
-            print("All Functions:")
-            for function_name in invoke_config['functions'].keys():
-                print('\t', function_name)
+            config_name = DEFAULT_CONFIG
+        invoke_config, password_config = handle_invoke_config(project_dir, config_name)
+        print("All Functions:")
+        for function_name in invoke_config['functions'].keys():
+            print('\t', function_name)
 
 
 

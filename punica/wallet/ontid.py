0c389926573646e357ef39a03fd430d606911259
lucas7788 <sishsh@163.com>
2018-10-19 12:42:54 +0800

    rejust account and ontid delete

--- punica/wallet/ontid.py
+++ punica/wallet/ontid.py
@@ -5,6 +5,21 @@ from punica.exception.punica_exception import PunicaException
 
 class OntId:
     @staticmethod
+    def delete_ont_id(project_dir: str, ont_id: str):
+        wallet_manager = Account.get_wallet_manager(project_dir)
+        try:
+            pwd = Account.get_password()
+            for identity in wallet_manager.wallet_in_mem.identities:
+                if identity.ont_id == ont_id:
+                    wallet_manager.wallet_in_mem.identities.remove(identity)
+                    wallet_manager.write_wallet()
+                    print('delete ', identity.ont_id + ' success')
+                    return
+            print("there is not the ontid,", ont_id)
+        except PunicaException:
+            pass
+
+    @staticmethod
     def add_ont_id(project_dir: str):
         wallet_manager = Account.get_wallet_manager(project_dir)
         try:

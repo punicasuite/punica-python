656310dd27c1c13de085280607b50a4099e03b98
dingxin <wdx7266@vip.qq.com>
2018-12-03 10:36:52 +0800

    update version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(getcwd(), 'README.md'), mode='r', encoding='utf-8') as f:
 
 setup(
     name='punica',
-    version='0.0.16',
+    version='0.0.17',
     description="""Ontology DApp Development Framework""",
     long_description=long_description,
     long_description_content_type="text/markdown",

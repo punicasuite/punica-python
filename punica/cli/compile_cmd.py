e7e69b879783bdb5eaadb9841ce1667a0ca48284
dingxin <wdx7266@vip.qq.com>
2019-05-31 16:33:03 +0800

    fix remote compile error

--- punica/cli/compile_cmd.py
+++ punica/cli/compile_cmd.py
@@ -12,48 +12,39 @@ from punica.compile.contract_compile import PunicaCompiler
 from punica.exception.punica_exception import PunicaException
 
 
-def compile_contract(contract_dir, contract_name, avm, abi, local):
+def compile_contract(contract_dir, contract_name, local):
     contract_path = os.path.join(contract_dir, contract_name)
-    print('Compile', contract_name)
-    if avm:
-        PunicaCompiler.generate_avm_file(contract_path)
-        print('\tGenerate avm file successful...')
-    if abi:
-        PunicaCompiler.generate_abi_file(contract_path)
-        print('\tGenerate abi file successful...')
-    if not avm and not abi:
-        PunicaCompiler.compile_contract(contract_path, local)
+    click.echo(f'Compile {contract_name}')
+    PunicaCompiler.compile_contract(contract_path, local)
+    click.echo('Generate avm file successful...')
 
 
 @main.command('compile')
 @click.option('--contracts', nargs=1, type=str, default='', help='Compile specified contracts files in contracts dir.')
-# @click.option('--avm', nargs=1, type=str, default=False, help='Only generate avm file flag.')
-# @click.option('--abi', nargs=1, type=str, default=False, help='Only generate abi file flag.')
 @click.option('--local', nargs=1, type=bool, default=False, help='Use local compiler.')
 @click.pass_context
-def compile_cmd(ctx, contracts, local):
+def compile_cmd(ctx, contracts, local: bool):
     """
     Compile the specified contracts to avm and abi file.
     """
     project_dir = ctx.obj['PROJECT_DIR']
     try:
-        print('Compile')
+        click.echo(f'Compiling...')
         if contracts != '':
             contract_file_path = os.path.join(project_dir, contracts)
             if not os.path.exists(contract_file_path):
                 if os.path.dirname(contracts) != '':
-                    print(contracts, 'not found')
+                    click.echo(f'{contracts} not founded...')
                     return
                 contract_file_path = os.path.join(project_dir, 'contracts', contracts)
                 if not os.path.exists(contract_file_path):
-                    print(contracts, 'not found')
+                    click.echo(f'{contracts} not founded...')
                     return
             if contracts.endswith('.py') or contracts.endswith('.cs'):
-                compile_contract(os.path.dirname(contract_file_path), os.path.basename(contract_file_path), False, False, local)
+                compile_contract(os.path.dirname(contract_file_path), os.path.basename(contract_file_path), local)
             else:
-                print('Compile Error')
-                print('file type is wrong')
-                os._exit(0)
+                click.echo('Valid file is required.')
+                exit(0)
         else:
             contract_dir = os.path.join(project_dir, 'contracts')
             contract_name_list = os.listdir(contract_dir)
@@ -72,19 +63,19 @@ def compile_cmd(ctx, contracts, local):
                         elif os.path.isfile(contract_file_path_sub):
                             if contract_file_path_sub.endswith('.py') or contract_file_path_sub.endswith('.cs'):
                                 compile_contract(os.path.dirname(contract_file_path_sub),
-                                                 os.path.basename(contract_file_path_sub), False, False, local)
+                                                 os.path.basename(contract_file_path_sub), local)
                             else:
-                                print('Compile Error')
+                                click.echo('Compile Error')
                                 print('file type is wrong')
-                                os._exit(0)
+                                exit(0)
                 elif os.path.isfile(contract_file_path):
+                    contract_dir = os.path.dirname(contract_file_path)
                     if contract_name.endswith('.py') or contract_name.endswith('.cs'):
-                        compile_contract(os.path.dirname(contract_file_path), os.path.basename(contract_file_path),
-                                         False, False, local)
+                        compile_contract(contract_dir, os.path.basename(contract_file_path), local)
                     else:
                         print('Compile Error')
                         print('file type is wrong')
-                        os._exit(0)
+                        exit(0)
                 else:
                     raise RuntimeError("contract path is wrong")
     except (PunicaException, SDKException) as e:
